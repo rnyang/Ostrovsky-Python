c77ceabb4999268c932e3897ea4565173c47bbe6
Runnan Yang <yang.runnan@gmail.com>
2012-08-13 23:28:23 -0500

    Added testing on basic example

--- main.py
+++ main.py
@@ -2,7 +2,22 @@
 
 import ostrovsky
 
+def utility(agent, contracts):
+	return 1
+
 def main():
-	print 'whee'
+	sys = ostrovsky.System()
+	agent0 = ostrovsky.Agent(sys, utility, 0, 0)
+	agent1 = ostrovsky.Agent(sys, utility, 1, 1)
+	agent2 = ostrovsky.Agent(sys, utility, 2, 2)
+	agents = [agent0, agent1, agent2]
+
+	contract0 = ostrovsky.Contract(sys, 1, 1, 0, 1, 0)
+	contract1 = ostrovsky.Contract(sys, 1, 1, 1, 2, 1)
+	contracts = [contract0, contract1]
+
+	sys.seed(agents, contracts)
+	sys.generateArrows()
+
 
 main()
