37694397ea12b57dd4361596e387535d431efa29
Runnan Yang <yang.runnan@gmail.com>
2012-09-01 23:09:49 -0500

    stuff

--- multilateral.py
+++ multilateral.py
@@ -10,8 +10,18 @@ class System:
 		self.agents = agents # [Agent]
 		self.ventures = ventures # [Venture]
 
-	def efficient():
-		# Maximize sum of utility of agents
+	def max_utility():
+		continue
+
+
+	# [Double] -> Double
+	# Allocation = [r_omega, r_psi]
+	def utility(allocation):
+		utility = 0
+		for agent in self.agents:
+			utility += agent.utility(allocation)
+
+		return utility
 
 class Agent:
 	def __init__(sys, utility, id_):
@@ -19,11 +29,21 @@ class Agent:
 		self.utility = utility # Valuation Function: [Double] -> Double
 		self.id = id_
 
+"""
+# Sample Utility Function
+# from Multilateral Matching pg. 7
+
+def utility_buyer(allocation):
+	utility = 32 * min(allocation[1], 50)
+	return utility
+
+"""
+
 class Venture:
 	def __init__(sys, r_max, id_):
 		self.sys = sys
-		self.r_max = r_max # Maximum Participation in Venture
-		self.id = id_
+		self.r_max = r_max # Maximum Participation in Venture : Double
+		self.id = id_ # Int
 
 def main():
 	print 'whee'
