38e9a4550beb75d2192fe42863381719f6a75d97
ry <ry@ubuntu.(none)>
2013-02-20 00:48:22 -0600

    working optimization of test case

--- multiSolver.py
+++ multiSolver.py
@@ -8,28 +8,28 @@ from time import sleep
 # x[1] = r_{\psi}
 
 def u(x):
-    u1 = -16*x
-    u2 = -10*x #-25*x[1] 
-    u3 = -2*x -(1/100)*np.power(x,2)
-    u4 = 60*x - (7/100)*np.power(x,2)
-    #u5 = 32 * min(x[1], 50)
+    u1 = -16*x[0]
+    u2 = -10*x[0] -25*x[1] 
+    u3 = -2*x[0] -(1./100.)*np.power(x[0],2)
+    u4 = 60*x[0] - (7./100.)*np.power(x[0],2)
+    u5 = 32 * min(x[1], 50)
     #u5 = 32 * x[1]
 
-    #u = -1 * (u1 + u2 + u3 + u4 + u5)
-    n = -1 * (u1 + u2 + u3 + u4)
-    n = -( 32*x - (8/100) * np.power(x,2))
+    u = -1 * (u1 + u2 + u3 + u4 + u5)
+    #n = -1 * (u1 + u2 + u3 + u4)
+    #n = -( 32*x - (8./100.) * np.power(x,2))
     
     print "x: ", x
-    print n
-    sleep(0.5)
+    print u
+    #sleep(0.25)
 
-    #if x[0] < 0 or x[1] < 0:
-    #    return 0
+    if x[0] < 0 or x[1] < 0:
+        return 0
 
-    return n
+    return u
 
 def main():
-    x0 = [200]
+    x0 = [0, 0]
     xopt = fmin(u, x0)
 
     print xopt
